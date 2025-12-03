d4e3751c6156e6a777b41ec868d4c277b67675a4
wakamonoo <joven.serdanbataller21@gmail.com>
2025-12-03 20:13:38 +0800

    ui changes

--- src/sections/footer.jsx
+++ src/sections/footer.jsx
@@ -3,10 +3,10 @@ import { SiTailwindcss, SiReact, SiNextdotjs } from "react-icons/si";
 
 export default function Footer() {
   return (
-    <div className="w-full m-4">
+    <div className="w-full m-4 flex items-center justify-center">
       <p className="text-center text-base text-normal font-normal">
         forged with the power of{" "}
-        <span className="inline-flex gap-2 text-base text-normal font-normal">
+        <span className="inline-flex gap-1 text-base px-2 text-normal font-normal">
           <SiNextdotjs /> <SiReact /> <SiTailwindcss />
         </span>{" "}
         | Copyright © 2025 wakamonoo
