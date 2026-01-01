a510324bd1a7cbd025132cdb0aff942d5a0f55f4
wakamonoo <joven.serdanbataller21@gmail.com>
2026-01-01 21:09:20 +0800

    changes on the buttons

--- src/components/buttons.jsx
+++ src/components/buttons.jsx
@@ -11,7 +11,9 @@ export default function NavMenu() {
   const [isScrolled, setIsScrolled] = useState(false);
   const [expanded, setExpanded] = useState(false);
 
-  {/* ———————————————————————————————————— menu on 50 scroll ——— */}
+  {
+    /* ———————————————————————————————————— menu on 50 scroll ——— */
+  }
   useEffect(() => {
     const handleScroll = () => {
       setIsScrolled(window.scrollY > 50);
@@ -21,11 +23,13 @@ export default function NavMenu() {
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
-  {/* ———————————————————————————————————— nav sec on !scroll ——— */}
+  {
+    /* ———————————————————————————————————— nav sec on !scroll ——— */
+  }
   if (!isScrolled) {
     return (
       <div className="flex left-1/2 transform -translate-x-1/2 p-4 fixed z-50">
-        <div className="flex text-4xl md:text-5xl gap-2 bg-panel p-4 px-6 rounded-full">
+        <div className="flex text-4xl md:text-5xl gap-2 bg-panel px-8 py-4 rounded-full">
           <NavLinks />
         </div>
       </div>
@@ -34,14 +38,11 @@ export default function NavMenu() {
   return (
     <div className="flex left-1/2 transform -translate-x-1/2 p-4 fixed z-50">
       {expanded ? (
-        <div className="flex text-4xl md:text-5xl gap-2 bg-panel p-4 rounded-2xl">
+        <div className="flex text-4xl md:text-5xl gap-2 bg-panel px-8 py-4 rounded-full">
           <NavLinks />
         </div>
       ) : (
-        <button
-          onClick={() => setExpanded(true)}
-          className="p-4 text-normal"
-        >
+        <button onClick={() => setExpanded(true)} className="p-4 text-normal">
           <MdApps className="text-4xl md:text-5xl text-normal cursor-pointer hover:scale-110 transition" />
         </button>
       )}
@@ -54,33 +55,49 @@ function NavLinks() {
     <>
       <a
         href="#hero"
-        className="text-normal transition-all duration-200 hover:text-[var(--color-accent)] hover:scale-110"
+        className="flex flex-col items-center justify-center group text-normal transition-all duration-200 hover:text-[var(--color-accent)] hover:scale-110"
       >
         <AiFillHome />
+        <p className="text-xs opacity-0 group-hover:opacity-100 transition-all duration-300">
+          Home
+        </p>
       </a>
+
       <a
         href="#about"
-        className="text-normal transition-all duration-200 hover:text-[var(--color-accent)] hover:scale-110"
+        className="flex flex-col items-center justify-center group text-normal transition-all duration-200 hover:text-[var(--color-accent)] hover:scale-110"
       >
         <VscAccount />
+        <p className="text-xs opacity-0 group-hover:opacity-100 transition-all duration-300">
+          About
+        </p>
       </a>
       <a
         href="#skills"
-        className="text-normal transition-all duration-200 hover:text-[var(--color-accent)] hover:scale-110"
+        className="flex flex-col items-center justify-center group text-normal transition-all duration-200 hover:text-[var(--color-accent)] hover:scale-110"
       >
         <RiRocket2Fill />
+        <p className="text-xs opacity-0 group-hover:opacity-100 transition-all duration-300">
+          Skills
+        </p>
       </a>
       <a
         href="#portfolio"
-        className="text-normal transition-all duration-200 hover:text-[var(--color-accent)] hover:scale-110"
+        className="flex flex-col items-center justify-center group text-normal transition-all duration-200 hover:text-[var(--color-accent)] hover:scale-110"
       >
         <CgWebsite />
+        <p className="text-xs opacity-0 group-hover:opacity-100 transition-all duration-300">
+          Portfolio
+        </p>
       </a>
       <a
         href="#contact"
-        className="text-normal transition-all duration-200 hover:text-[var(--color-accent)] hover:scale-110"
+        className="flex flex-col items-center justify-center group text-normal transition-all duration-200 hover:text-[var(--color-accent)] hover:scale-110"
       >
         <FaEnvelope />
+        <p className="text-xs opacity-0 group-hover:opacity-100 transition-all duration-300">
+          Contact
+        </p>
       </a>
     </>
   );
